098705d21f8276af56bcde255afcbb1b394070ba
Poespas <jonathanvis148@gmail.com>
2018-06-26 21:59:18 +0200

    123
    123

--- trade.js
+++ trade.js
@@ -1,26 +1,3 @@
-const TradeOfferManagner = require ('steam-tradeoffer-manager');
-const SteamUser = require('steam-user');
-const SteamCommunity = require('steamcommunity');
-
-const community = new SteamCommunity();
-const client = new SteamUser();
-const manager = new TradeOfferManagner({
-	steam: client,
-	community: community,
-	language: 'en'
-});
-
-function handletrade (trade, app) {
-    if (trade.partner === '76561198132844160') {
-    offer.accept();
-    console.log('admin trade');
-  } else {
-    doOtherStuff
-    }
+module.exports.test = function () {
+	console.log("sup");
 }
-
-
-exports.printMsg = function() {
-  console.log("This is a message from the demo package");
-}
-
