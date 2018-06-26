77d3c72078d5494c1373c83517b00aa907875982
Enzotoy <39483959+Enzotoy@users.noreply.github.com>
2018-06-26 21:51:22 +0200

    Update cours.js

--- cours.js
+++ cours.js
@@ -1,11 +1,19 @@
 const SteamUser = require('steam-user');
 const SteamTotp = require('steam-totp');
+const SteamCommunity = require('steamcommunity');
+const TradeOfferManagner = require ('steam-tradeoffer-manager');
 const Request = require ('Request');
 
-const trade = require ('./trade.js');
-const config = require ('./config.json');
+const config = require('./config.json');
+const trade = require('./trade.js');
 
 const client = new SteamUser();
+const community = new SteamCommunity();
+const manager = new TradeOfferManagner({
+	steam: client,
+	community: community,
+	language: 'en'
+});
 
 const logOnOptions = {
   accountName: (config.name),
@@ -21,3 +29,21 @@ client.on('loggedOn', () => {
   client.setPersona(SteamUser.Steam.EPersonaState.Online);
   client.gamesPlayed(440);
 });
+
+	client.on('webSession', (sessionid, cookies) => {
+		manager.setCookies(cookies);
+
+		community.setCookies(cookies);
+		community.startConfirmationChecker(10000, '');
+
+});
+
+	client.on('friendRelationship', (steamid, relationship) => {
+		if (relationship === 2) {
+			client.addFriend(steamid);
+			client.chatMessage(steamid, "Hello, i'm a bot ! Thank's for adding me ! If you have any question, add my owner. the link of my owner profile is on my profile ! :)")
+			console.log("J'ai ajouté quelqu'un en ami")
+		}
+	});
+
+trade.printMsg();
